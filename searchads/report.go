c4b359086435b6483ee883e4b779bab86035b251
pavelm <pavelm@kidsacademy.com>
2019-09-06 13:52:26 +0300

    change serving status

--- searchads/report.go
+++ searchads/report.go
@@ -31,7 +31,7 @@ type CampaignMetadata struct {
 	CampaignStatus                     Status            `json:"campaignStatus"`
 	App                                App               `json:"app"`
 	ServingStatus                      ServingStatus     `json:"servingStatus"`
-	ServingStateReasons                *string           `json:"servingStateReasons"`
+	ServingStateReasons                []string          `json:"servingStateReasons"`
 	CountriesOrRegions                 []CountryCode     `json:"countriesOrRegions"`
 	ModificationTime                   string            `json:"modificationTime"`
 	TotalBudget                        Amount            `json:"totalBudget"`
